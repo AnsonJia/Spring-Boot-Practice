082d18779077196fe0f02a9c3397b0db2b1fcceb
Anson Jia <ansonjia123@gmail.com>
2025-05-13 20:09:26 -0400

    Moved into 'Basics' folder

--- src/main/java/com/SpringPractice/store/PaymentService/Stripe.java
+++
@@ -1,9 +0,0 @@
-package com.SpringPractice.store.PaymentService;
-
-public class Stripe implements PaymentService {
-    @Override
-    public void processPayment(double amount){
-        System.out.println("STRIPE");
-        System.out.println("Amount: "+ amount);
-    }
-}
