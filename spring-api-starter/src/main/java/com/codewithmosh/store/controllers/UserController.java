1ea583abe5566d6edc3b10ae5eed022e3b56b909
Anson Jia <ansonjia123@gmail.com>
2025-06-02 10:17:04 -0400

    Updated DTO to id path

--- spring-api-starter/src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ spring-api-starter/src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -33,11 +33,13 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
         var user = userRepository.findById(id).orElse( null);
         if (user == null){
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(user);
+
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 }
