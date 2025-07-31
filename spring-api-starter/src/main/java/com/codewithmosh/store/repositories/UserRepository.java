19e04069dc92d77bf10741f349bab0b59555e149
Anson Jia <ansonjia123@gmail.com>
2025-07-30 22:37:09 -0400

    added comment

--- spring-api-starter/src/main/java/com/codewithmosh/store/repositories/UserRepository.java
+++ spring-api-starter/src/main/java/com/codewithmosh/store/repositories/UserRepository.java
@@ -4,5 +4,6 @@ import com.codewithmosh.store.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+//jpa repository for lists, crud for iterable
 public interface UserRepository extends JpaRepository<User, Long> {
 }
