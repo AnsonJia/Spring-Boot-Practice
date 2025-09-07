c9992dfacf41b342ee05d0cd81de72caa9de198f
Anson Jia <ansonjia123@gmail.com>
2025-09-06 21:47:24 -0400

    example using application.properties values in code

--- Basics/src/main/java/com/SpringPractice/store/HomeController.java
+++ Basics/src/main/java/com/SpringPractice/store/HomeController.java
@@ -1,12 +1,18 @@
 package com.SpringPractice.store;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller // handel web requests
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
     @RequestMapping("/")
     public String index(){
+        System.out.println("appName: " + appName);
         return "index.html";
     }
 }
