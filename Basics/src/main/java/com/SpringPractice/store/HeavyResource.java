75a2f693255d775a5b9613e5f344c225bd8f3410
Anson Jia <ansonjia123@gmail.com>
2026-04-26 21:53:20 -0400

    updated comments for heavy resource example

--- Basics/src/main/java/com/SpringPractice/store/HeavyResource.java
+++ Basics/src/main/java/com/SpringPractice/store/HeavyResource.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Lazy // creates objects only when needed
+@Lazy // creates/delays objects until only when needed
 public class HeavyResource {
     public HeavyResource() {
         System.out.println("Heavy Resource created");
