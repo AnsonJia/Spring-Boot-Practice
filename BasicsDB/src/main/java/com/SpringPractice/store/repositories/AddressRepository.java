45385c7d14bb48f608a7615e7c612fc2442b6921
Anson Jia <ansonjia123@gmail.com>
2026-05-01 22:03:25 -0400

    updated to use java jdk 21 instead of 24

---
+++ BasicsDB/src/main/java/com/SpringPractice/store/repositories/AddressRepository.java
@@ -0,0 +1,4 @@
+package com.SpringPractice.store.repositories;
+
+public interface AddressRepository extends org.springframework.data.repository.CrudRepository<com.SpringPractice.store.entities.Address, java.lang.Long> {
+}
---
+++ BasicsDB/src/main/java/com/SpringPractice/store/repositories/UserRepository.java
@@ -0,0 +1,4 @@
+package com.SpringPractice.store.repositories;
+
+public interface UserRepository {
+}
