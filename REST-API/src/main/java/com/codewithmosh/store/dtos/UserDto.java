5f95137a9a8758122d98551f338a4efdbce1c531
Anson Jia <ansonjia123@gmail.com>
2026-06-12 11:14:31 -0400

    added comments for custom response data

--- REST-API/src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ REST-API/src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,13 +1,26 @@
 package com.codewithmosh.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor //needs a constructor for the fields
 @Getter// so spring can read the data and create JSON object
 public class UserDto {
     //add fields we want to expose to the outside
+    //@JsonIgnore// to ignore the id field and not expose it to clients (if we want to hide it)
+    //@JsonProperty("user_id")// to change the name of the field in the JSON response (if we want to rename it)
     private Long id;
     private String name;
     private String email;
+    //@JsonInclude(JsonInclude.Include.NON_NULL)//only include field in response if not null (if we want to conditionally include it)
+    //private String phoneNumber; //phone num col doesn't exist in db, so it will be null
+    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")//change the format of the date, time, numbers in the JSON response
+    //private LocalDateTime createdAt;
 }
--- REST-API/src/main/java/com/codewithmosh/store/mappers/UserMapper.java
+++ REST-API/src/main/java/com/codewithmosh/store/mappers/UserMapper.java
@@ -3,9 +3,12 @@ package com.codewithmosh.store.mappers;
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 //MapStruct will automatically implement this interface at runtime (target/generated-sources/annotations folder)
 @Mapper(componentModel = "spring") //tells spring to manage the lifecycle of this mapper and inject it where needed
 public interface UserMapper {
+    //customize mapping code and valid string java expression java(java expression)
+    //@Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")//find local date time now
     UserDto toDto(User user);//method to map user to userDto
 }
