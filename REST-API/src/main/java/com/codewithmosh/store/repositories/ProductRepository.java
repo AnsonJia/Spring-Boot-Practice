0e7c4956b27065a32028dbcee084ffbbc8bb00c5
Anson Jia <ansonjia123@gmail.com>
2026-06-12 18:03:05 -0400

    added custom queries to ProductRepository for the product endpoint

--- REST-API/src/main/java/com/codewithmosh/store/repositories/ProductRepository.java
+++ REST-API/src/main/java/com/codewithmosh/store/repositories/ProductRepository.java
@@ -1,7 +1,17 @@
 package com.codewithmosh.store.repositories;
 
 import com.codewithmosh.store.entities.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @EntityGraph(attributePaths = "category")//tells spring to load the category along with the product
+    List<Product> findByCategoryId(Byte categoryId); //filter products by category
+
+    @EntityGraph(attributePaths = "category")// to fetch the category along with the product
+    @Query("SELECT p FROM Product p")// JOIN FETCH p.category")//JPQL query to select all products and their category
+    List<Product> findAllWithCategory(); //find all products after joining the category with products
 }
