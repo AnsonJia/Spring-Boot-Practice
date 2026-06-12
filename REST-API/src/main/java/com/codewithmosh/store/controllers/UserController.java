0e6498b124290d85aa5077eed9264c416a21713e
Anson Jia <ansonjia123@gmail.com>
2026-06-12 09:52:04 -0400

    updated UserController to use MapStruct mapper

--- REST-API/src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ REST-API/src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,6 +19,8 @@ import java.util.List;
 @RequestMapping("/users") //mapping controller to the users endpoint to simplify url endpoint code
 public class UserController {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;// add userMapper field
+
     // method: GET, POST, PUT, DELETE
     //@RequestMapping("/users") //default uses the GET method (GetMapping also works and is more consice/shorter)
     @GetMapping
@@ -27,7 +30,9 @@ public class UserController {
         return userRepository.findAll()
                 .stream()
                 //map user to user dto and convert to list (if we change user entity, we just need to update mapping)
-                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+                //.map(user -> new UserDto(user.getId(), user.getName(), user.getEmail())) //manual mapping user to userDto can become repetitive
+                //instead better to use mapper to dto using MapStruct (user -> userMapper.toDto(user))
+                .map(userMapper::toDto)
                 .toList();//get all users in database
     };
     //get user by their id given by the path variable/url param
@@ -41,7 +46,11 @@ public class UserController {
         //if not null, return status 200 and the user
         //return new ResponseEntity<>(user, HttpStatus.OK);
         //return ResponseEntity.ok(user); //exposes all the data to clients (use DTOs)
-        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail()); //first create userDto and put in response
-        return ResponseEntity.ok(userDto);
+
+        //var userDto = new UserDto(user.getId(), user.getName(), user.getEmail()); //first create userDto and put in response
+        //return ResponseEntity.ok(userDto); //only exposes the dto fields
+
+        // no need to create a userDto for mapper
+        return ResponseEntity.ok(userMapper.toDto(user));
     }
 }
