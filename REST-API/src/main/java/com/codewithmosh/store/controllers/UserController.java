34de0d4637691f976139fb5636e92825a1d65a66
Anson Jia <ansonjia123@gmail.com>
2026-06-12 12:18:47 -0400

    added sortBy query parameter to getAllUsers

--- REST-API/src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ REST-API/src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,18 +1,14 @@
 package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.UserDto;
-import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor//lombok constructor
@@ -24,17 +20,27 @@ public class UserController {
     // method: GET, POST, PUT, DELETE
     //@RequestMapping("/users") //default uses the GET method (GetMapping also works and is more consice/shorter)
     @GetMapping
-    public Iterable<UserDto> getAllUsers() { //repository returns an Iterable object
+    public Iterable<UserDto> getAllUsers(
+            //annotation for a query request param (set required to false for optional and if no sort field provided, default is empty string)
+            @RequestParam(required = false, defaultValue = "", name = "sort") String sortBy //name binds query parameter "sort" to method parameter sortBy
+    ) { //repository returns an Iterable object
+
+        //check if sort field is valid, otherwise sort by name
+        if (!Set.of("name", "email").contains(sortBy)) //because contains cannot take null, set default value to empty string
+            sortBy = "name";
+
         //return userRepository.findAll(); //exposes all the data to clients (use DTOs)
         //use the stream api to map user objects to user dtos
-        return userRepository.findAll()
+        return userRepository.findAll(Sort.by(sortBy))//pass sort.by the sort field as an argument
                 .stream()
                 //map user to user dto and convert to list (if we change user entity, we just need to update mapping)
                 //.map(user -> new UserDto(user.getId(), user.getName(), user.getEmail())) //manual mapping user to userDto can become repetitive
                 //instead better to use mapper to dto using MapStruct (user -> userMapper.toDto(user))
                 .map(userMapper::toDto)
-                .toList();//get all users in database
+                .toList();//converts stream to list
     };
+
+
     //get user by their id given by the path variable/url param
     @GetMapping("/{id}")//because id is a param, we need {} and @PathVariable for the variable
     public ResponseEntity<UserDto> getUserById(@PathVariable Long id) { //response entity allows us to customize our responses
