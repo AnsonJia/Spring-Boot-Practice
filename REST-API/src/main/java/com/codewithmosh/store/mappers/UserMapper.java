b23b93efba7045f8291c8358fd53d85441f66142
Anson Jia <ansonjia123@gmail.com>
2026-06-14 08:45:00 -0400

    added toEntity method to UserMapper for post requests

--- REST-API/src/main/java/com/codewithmosh/store/mappers/UserMapper.java
+++ REST-API/src/main/java/com/codewithmosh/store/mappers/UserMapper.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store.mappers;
 
+import com.codewithmosh.store.dtos.RegisterUserRequest;
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import org.mapstruct.Mapper;
@@ -10,5 +11,7 @@ import org.mapstruct.Mapping;
 public interface UserMapper {
     //customize mapping code and valid string java expression java(java expression)
     //@Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")//find local date time now
-    UserDto toDto(User user);//method to map user to userDto
+    UserDto toDto(User user);//method to map user to userDto for getting data
+
+    User toEntity(RegisterUserRequest request);//method to map post request to user entity for posting data
 }
