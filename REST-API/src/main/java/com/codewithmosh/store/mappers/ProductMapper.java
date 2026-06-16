48bf9dbf336a21a606bbca1d262c12a469a1ecac
Anson Jia <ansonjia123@gmail.com>
2026-06-15 20:15:05 -0400

    added toEntity and update methods for product post and put endpoints

--- REST-API/src/main/java/com/codewithmosh/store/mappers/ProductMapper.java
+++ REST-API/src/main/java/com/codewithmosh/store/mappers/ProductMapper.java
@@ -4,9 +4,15 @@ import com.codewithmosh.store.dtos.ProductDto;
 import com.codewithmosh.store.entities.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     @Mapping(source = "category.id", target = "categoryId")//maps category id to categoryId in productDto
     ProductDto toDto(Product product);
+
+    Product toEntity(ProductDto productDto);//map dto to a product entity object
+
+    @Mapping(target = "id",  ignore = true) //tell mapstruct to ignore id field when updating an existing product (we don't need to update id)
+    void update(ProductDto productDto, @MappingTarget Product product);
 }
