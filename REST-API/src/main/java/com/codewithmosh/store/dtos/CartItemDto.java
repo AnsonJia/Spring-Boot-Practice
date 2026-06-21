33be5b3a7a4fd99aca83e56eb8d86ea8f7e13062
Anson Jia <ansonjia123@gmail.com>
2026-06-20 22:00:47 -0400

    added response fields to CartItemDto

--- REST-API/src/main/java/com/codewithmosh/store/dtos/CartItemDto.java
+++ REST-API/src/main/java/com/codewithmosh/store/dtos/CartItemDto.java
@@ -1,4 +1,25 @@
 package com.codewithmosh.store.dtos;
 
-public class CartItemDto {
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CartItemDto { //new dto for addToCart endpoint response
+    private CartProductDto product; //new dto for including product details in cart item response
+    private int quantity;
+    private BigDecimal totalPrice;
+
+    /* ex response
+    {
+        "product": {
+            "id": 1,
+            "name": "Product 1",
+            "price": 10.00
+        },
+        "quantity": 2,
+        "totalPrice": 20.00
+    }
+     */
+
 }
