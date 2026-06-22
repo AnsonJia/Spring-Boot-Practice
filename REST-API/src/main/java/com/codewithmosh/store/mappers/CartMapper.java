b78b06d5bf0419612c9b9e1c904f2352b00b450a
Anson Jia <ansonjia123@gmail.com>
2026-06-21 21:40:37 -0400

    updated mapper with toDto for cartItem

--- REST-API/src/main/java/com/codewithmosh/store/mappers/CartMapper.java
+++ REST-API/src/main/java/com/codewithmosh/store/mappers/CartMapper.java
@@ -1,10 +1,17 @@
 package com.codewithmosh.store.mappers;
 
 import com.codewithmosh.store.dtos.CartDto;
+import com.codewithmosh.store.dtos.CartItemDto;
 import com.codewithmosh.store.entities.Cart;
+import com.codewithmosh.store.entities.CartItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
     CartDto toDto(Cart cart);
+
+    //map total price to get total price from cart item entity
+    @Mapping(target = "totalPrice", expression = "java(cartItem.getTotalPrice())")// dto field maps to the method in cartItem
+    CartItemDto toDto(CartItem cartItem);
 }
