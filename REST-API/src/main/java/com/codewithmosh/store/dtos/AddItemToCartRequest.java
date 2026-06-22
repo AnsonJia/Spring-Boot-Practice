d1a896de265b2f38b96567297674ca0f0511a997
Anson Jia <ansonjia123@gmail.com>
2026-06-22 16:54:58 -0400

    updated @NotNull annotation with message

--- REST-API/src/main/java/com/codewithmosh/store/dtos/AddItemToCartRequest.java
+++ REST-API/src/main/java/com/codewithmosh/store/dtos/AddItemToCartRequest.java
@@ -5,6 +5,6 @@ import lombok.Data;
 
 @Data
 public class AddItemToCartRequest {//request body for add item to cart endpoint
-    @NotNull //make sure productId is not null in request body
+    @NotNull(message = "Product ID is required") //make sure productId is not null in request body
     private Long productId; //we only need to have productId in request
 }
