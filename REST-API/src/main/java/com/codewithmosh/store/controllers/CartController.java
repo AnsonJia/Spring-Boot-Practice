d4b2ae0f8639f3f356e7c78e755ee43d8e5f8770
Anson Jia <ansonjia123@gmail.com>
2026-06-21 21:38:54 -0400

    added addToCart post endpoint

--- REST-API/src/main/java/com/codewithmosh/store/controllers/CartController.java
+++ REST-API/src/main/java/com/codewithmosh/store/controllers/CartController.java
@@ -1,17 +1,21 @@
 package com.codewithmosh.store.controllers;
 
+import com.codewithmosh.store.dtos.AddItemToCartRequest;
 import com.codewithmosh.store.dtos.CartDto;
+import com.codewithmosh.store.dtos.CartItemDto;
 import com.codewithmosh.store.entities.Cart;
+import com.codewithmosh.store.entities.CartItem;
 import com.codewithmosh.store.mappers.CartMapper;
 import com.codewithmosh.store.repositories.CartRepository;
+import com.codewithmosh.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
@@ -19,6 +23,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class CartController {
     private final CartRepository cartRepository;
     private final CartMapper cartMapper;
+    private final ProductRepository productRepository;
 
     @PostMapping
     public ResponseEntity<CartDto> createCart(
@@ -32,4 +37,39 @@ public class CartController {
         //return new ResponseEntity<>(cartDto, HttpStatus.CREATED); //we could also do it this way without uri location header
         return ResponseEntity.created(uri).body(cartDto); //URIs are useful for public APIs
     }
+
+    @PostMapping("/{cartId}/items")
+    public ResponseEntity<CartItemDto> addToCart(
+            @PathVariable UUID cartId,
+            @RequestBody AddItemToCartRequest request //request dto for adding items to cart (product id)
+    ){
+        var cart = cartRepository.findById(cartId).orElse(null); //check if cartId is valid/exists
+        if (cart == null) {
+            return ResponseEntity.notFound().build(); //if not found return 404 not found
+        }
+        var product = productRepository.findById(request.getProductId()).orElse(null);//validate product id from request
+        if (product == null) {
+            return ResponseEntity.badRequest().build();//if null return 400 bad request not 404 because client sent invalid data in request
+        }
+        //check if the product is already in the cart (use stream to perform operations on a collection)
+        var cartItem = cart.getCartItems().stream()
+                .filter(item -> item.getProduct().getId().equals(product.getId()))
+                .findFirst()
+                .orElse(null);
+        if  (cartItem != null) {
+            cartItem.setQuantity(cartItem.getQuantity() + 1); //if item already in cart, increase quantity by 1
+        }else {
+            cartItem = new CartItem(); //if item not in cart, create new cart item and add to cart
+            cartItem.setProduct(product); //set product
+            cartItem.setQuantity(1); //set quantity
+            cartItem.setCart(cart); //set cart/update cart side of relationship (associate the cart item with the cart)
+            cart.getCartItems().add(cartItem); //add cart item to the collection in the cart object
+        }
+        //instead of saving to a cartItem repository, save to cart repository (item is dependent on carts)
+        cartRepository.save(cart);//the cart item will also be saved because of the cascade type we set in the cart entity (cascade = CascadeType.MERGE)
+        var cartItemDto = cartMapper.toDto(cartItem);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
+    }
+
 }
