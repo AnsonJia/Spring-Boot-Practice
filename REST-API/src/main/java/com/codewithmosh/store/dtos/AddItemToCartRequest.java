902b8c722814caa6d20499be35125f44ed803fa9
Anson Jia <ansonjia123@gmail.com>
2026-06-21 21:37:32 -0400

    added request dto for adding item to cart

---
+++ REST-API/src/main/java/com/codewithmosh/store/dtos/AddItemToCartRequest.java
@@ -0,0 +1,10 @@
+package com.codewithmosh.store.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class AddItemToCartRequest {//request body for add item to cart endpoint
+    @NotNull //make sure productId is not null in request body
+    private Long productId; //we only need to have productId in request
+}
