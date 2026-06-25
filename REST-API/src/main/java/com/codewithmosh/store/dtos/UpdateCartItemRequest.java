3d59f03a523f3dfa4dc6866b5b5b2a0ef4128365
Anson Jia <ansonjia123@gmail.com>
2026-06-24 22:39:07 -0400

    added comment

--- REST-API/src/main/java/com/codewithmosh/store/dtos/UpdateCartItemRequest.java
+++ REST-API/src/main/java/com/codewithmosh/store/dtos/UpdateCartItemRequest.java
@@ -10,5 +10,5 @@ public class UpdateCartItemRequest {
     @NotNull(message = "Quantity must be provided")
     @Min(value = 1, message = "Quantity must be greater than zero")
     @Max(value = 1000, message = "Quantity less than or equal to 1000")
-    private Integer quantity;
+    private Integer quantity; //we don't use primitive int type because it is default non-nullable so we cant provide a not null message
 }
