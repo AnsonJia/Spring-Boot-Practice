1511fd2a8846b5cdd27d678f72f92cafadf20d21
Anson Jia <ansonjia123@gmail.com>
2026-06-28 20:59:25 -0400

    added exception handler for product not found exceptions

---
+++ REST-API/src/main/java/com/codewithmosh/store/exceptions/ProductNotFoundException.java
@@ -0,0 +1,5 @@
+package com.codewithmosh.store.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {//custom exception type that inherits runtime exception
+    //we don't need extra data or methods right now because its only job is a label to identify this specific error
+}
