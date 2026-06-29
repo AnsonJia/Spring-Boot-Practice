fea8cfb0372a09f15e92e1b46e0f0b82c2a7f444
Anson Jia <ansonjia123@gmail.com>
2026-06-28 21:00:00 -0400

    added exception handler for cart not found exceptions

---
+++ REST-API/src/main/java/com/codewithmosh/store/exceptions/CartNotFoundException.java
@@ -0,0 +1,5 @@
+package com.codewithmosh.store.exceptions;
+
+public class CartNotFoundException extends RuntimeException { //custom exception type that inherits runtime exception
+    //we don't need extra data or methods right now because its only job is a label to identify this specific error
+}
