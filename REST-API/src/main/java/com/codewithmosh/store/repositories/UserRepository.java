32307735b524cbade07ff5f23b7575bcad6c0dcb
Anson Jia <ansonjia123@gmail.com>
2026-06-29 20:04:55 -0400

    added findByEmail derived query method to find users by their email

--- REST-API/src/main/java/com/codewithmosh/store/repositories/UserRepository.java
+++ REST-API/src/main/java/com/codewithmosh/store/repositories/UserRepository.java
@@ -4,7 +4,12 @@ import com.codewithmosh.store.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 //using JpaRepository so findAll returns a list
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email); //derived query method
+
+    //derived query that returns a user or empty (Optional<> because may not find user)
+    Optional<User> findByEmail(String email); // Optional<> also allows us to use .orElse(null)
 }
