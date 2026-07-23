c5f80e877003f05df5b5adb2fdd351dcf8f4d202
Anson Jia <ansonjia123@gmail.com>
2026-07-23 11:08:50 -0400

    updated security filter and added exception handling for unauthorised users

--- REST-API/src/main/java/com/codewithmosh/store/config/SecurityConfig.java
+++ REST-API/src/main/java/com/codewithmosh/store/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,6 +19,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration //marks 3rd party class and tells spring it contains @Bean methods so spring can create instances at runtime
@@ -56,11 +58,15 @@ public class SecurityConfig {//at runtime when spring creates an instance of thi
                         .requestMatchers("/carts/**").permitAll()//all endpoints matching the url will be public (** for all children)
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()//only register user endpoint (post) is public
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()//only login endpoint (post) is public
-                        //.requestMatchers(HttpMethod.POST, "/auth/validate").permitAll()// allow post request to validate endpoint
+                        .requestMatchers(HttpMethod.POST, "/auth/refresh").permitAll()//allow refresh endpoint to be public (otherwise requires access token which may be expired)
                         .anyRequest().authenticated()//all other requests needs to be authenticated
                         //.anyRequest().permitAll() //all endpoints will be public
                 )//authorize http requests
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);//add the filter to be the first in the chain
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)//add the filter to be the first in the chain
+                .exceptionHandling(c -> //takes a lambda function
+                        c.authenticationEntryPoint(//if user is not authenticated return 401 unauthorized (default is 403)
+                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
+
         return http.build(); //build the securityFilterChain object and return it
     }
 }
