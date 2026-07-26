2f58cebc601cd095b101672ee896d33656739c94
Anson Jia <ansonjia123@gmail.com>
2026-07-25 22:02:31 -0400

    added enum class for roles options

---
+++ REST-API/src/main/java/com/codewithmosh/store/entities/Role.java
@@ -0,0 +1,7 @@
+package com.codewithmosh.store.entities;
+
+// Represents the valid user roles in the application.
+public enum Role {//Using an enum restricts the role to the predefined values only
+    USER,
+    ADMIN
+}
