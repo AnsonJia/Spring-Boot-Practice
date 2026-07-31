72f04fafa5aae5e7aee03981f7b40c551fd7e6cd
Anson Jia <ansonjia123@gmail.com>
2026-07-30 23:00:28 -0400

    added admin controller with test endpoint for role based authorization

---
+++ REST-API/src/main/java/com/codewithmosh/store/controllers/AdminController.java
@@ -0,0 +1,14 @@
+package com.codewithmosh.store.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admim")
+public class AdminController {
+    @GetMapping("/hello")//test endpoint for admin role based authorization
+    public String sayHello() {
+        return "Hello Admin";
+    }
+}
