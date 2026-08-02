c105877f700709e1918baaed4f10d98d6f87926b
Anson Jia <ansonjia123@gmail.com>
2026-08-02 07:54:28 -0400

    updated authorities in authentication token

--- REST-API/src/main/java/com/codewithmosh/store/filters/JwtAuthenticationFilter.java
+++ REST-API/src/main/java/com/codewithmosh/store/filters/JwtAuthenticationFilter.java
@@ -7,12 +7,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @AllArgsConstructor//all args constructor for JwtService
@@ -33,14 +35,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {//call once p
             filterChain.doFilter(request, response);//let spring security process the request
             return;
         }
+
+        var role = jwtService.getRoleFromToken(token);
+        var userId = jwtService.getUserIdFromToken(token);
         //at this point it is a valid token
         var authentication = new UsernamePasswordAuthenticationToken( //create an authentication object
                 //userpassauthtoken has 2 implementation (principle, credentials) and (principle, credentials, authorities)
                 //first is used for login passing username/email/user and password, second is used for authenticated user
-                jwtService.getUserIdFromToken(token),
+                userId,
                 null,
-                null
-        ); //when representing authenticated user, we don't need credentials and ignore authorities for now
+                List.of(new SimpleGrantedAuthority("ROLE_"+role))//authorities (roles {roles need prefix ROLE_} or permissions)
+        ); //when representing already authenticated user, we don't need credentials
 
         authentication.setDetails(//boilerplate code
                 new WebAuthenticationDetailsSource().buildDetails(request)//attaching extra metadata to auth object
